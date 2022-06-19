2e4b05125f1d8880258ca80dab53d9d08fb7982f
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2022-06-19 23:45:00 +0900

    大幅修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+   modules: ['@nuxtjs/color-mode'],
     css: ["@/assets/css/main.css"],
     build: {
       postcss: {
@@ -12,5 +13,16 @@ export default defineNuxtConfig({
           },
         },
       },
+    },
+
+    colorMode: {
+      preference: 'light', 
+      dataValue: 'theme',
+    },
+
+    head:{
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" }, 
+      ],
     },
   });
